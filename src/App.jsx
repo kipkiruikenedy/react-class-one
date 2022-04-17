3e8168d89a4ad56b4e97b05d8b8fc374d0f94518
kipkiruikenedy <Kipkiruikenedy@gmail.com>
2022-04-17 16:35:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import Header from './components/Header'
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+function header() {
+  return (
+    <div className='header'>
+      <div className='logo'>
+ logo
+      </div>
+      <div className='menu'>
+menu
+      </div>
+    </div>
+  )
+}
+
+export default header
